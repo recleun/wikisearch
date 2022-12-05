7924dc059e6a55a31b7e6c4eaf6b2916c86c7668
Recleun <98621222+Recleun@users.noreply.github.com>
2022-12-05 02:34:14 +0200

    replace: compiled TypeScript

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 #! /usr/bin/env node
 const { program } = require("commander");
 const wiki = require("wikipedia");
-
 program
-	.name("wikisearch")
-	.description("A tool to quickly search something on wikipedia.")
-	.version("1.0.0", "-v, --version", "Shows the current version.");
-
+    .name("wikisearch")
+    .description("A tool to quickly search something on wikipedia.")
+    .version("1.0.0", "-v, --version", "Shows the current version.");
 program
-	.option("-d, --debug", "Debug mode.")
-	.requiredOption("-s, --searchword <text...>", "The search word.");
-
+    .option("-d, --debug", "Debug mode.")
+    .requiredOption("-s, --searchword <text...>", "The search word.");
 program.parse();
-
 const options = program.opts();
-if (options.debug) console.log(options);
+if (options.debug)
+    console.log(options);
 let search = '';
-let i=0;
-for (word of options.searchword) {
-	let added=false;
-	if (search=='') {
-		search+=word;
-		added=true;
-	}
-	if (!added) {
-		search+=' '+word;
-	}
+let i = 0;
+for (let word of options.searchword) {
+    let added = false;
+    if (search == '') {
+        search += word;
+        added = true;
+    }
+    if (!added) {
+        search += ' ' + word;
+    }
 }
-
 (async () => {
-	try {
-		const res = await wiki.summary(search);
-		const title = res.title;
-		if (title == "Not found.") {
-			console.log("\nNo results found.");
-			process.exit();
-		}
-		const description = res.description;
-		const summary = res.extract;
-		console.log("\nTitle: ", title);
-		console.log("------------");
-		console.log("Description: ", description ? description : "Not found.");
-		console.log("------------");
-		console.log("Summary: ", summary ? summary : "Not found.");
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        const res = await wiki.summary(search);
+        const title = res.title;
+        if (title == "Not found.") {
+            console.log("\nNo results found.");
+            process.exit();
+        }
+        const description = res.description;
+        const summary = res.extract;
+        console.log("\nTitle: ", title);
+        console.log("------------");
+        console.log("Description: ", description ? description : "Not found.");
+        console.log("------------");
+        console.log("Summary: ", summary ? summary : "Not found.");
+    }
+    catch (error) {
+        console.log(error);
+    }
 })();
