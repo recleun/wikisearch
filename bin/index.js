cad5e9c42cdb6bedf1d4273dc451eddcb45e1488
recleun <ali.muhammed7734@gmail.com>
2022-12-23 19:00:37 +0200

    add: disamiguation checker, debugging levels, related option

--- bin/index.js
+++ bin/index.js
@@ -11,14 +11,16 @@ program
     .version("1.1.5", "-v, --version", "Shows the current version.");
 
 program
-    .option("-d, --debug", "Debug mode.")
+    .option("-d, --debug <level>", "Debug mode.", "0")
     .option("-b, --browser", "Open the summary in your browser.")
     .option("-c, --copy", "Copy the summary if there is one.")
+    .option("-r, --related", "Search for related summaries with the search word.")
     .requiredOption("-s, --searchword <text...>", "The search word.");
 
 program.parse();
 const options = program.opts();
-if (options.debug) console.log(options);
+if (options.debug != "0") console.log(options);
+process.stdout.write("\n");
 
 let search = '';
 for (let word of options.searchword) {
@@ -32,28 +34,45 @@ for (let word of options.searchword) {
     }
 }
 
-let res;
-try {
-    res = await wiki.summary(search);
-} catch (error) {
-    console.error(error);
-}
-
-const title = res.title;
-const summary = res.extract;
-if (options.copy) {
-    clipboard.writeSync(summary);
-}
-
-if (!title || title == "Not found.") {
-    console.log("\nNo results found.");
-} else if (title != "Not found." && options.browser) {
-    await open(res.content_urls.desktop.page);
+if (options.related) {
+    let res;
+    try {
+        res = await wiki.related(search);
+        if (options.debug == "2") console.log(res);
+    } catch (error) {
+        console.error(error);
+    }
+    for (let [key, page] of Object.entries(res.pages)) {
+        console.log('- '+page.title.replace("_(disambiguation)", "").replaceAll("_", " "))
+    }
 } else {
-    const description = res.description;
-    console.log(chalk.italic.green("\nTitle: "), title);
-    console.log("------------");
-    console.log(chalk.italic.green("Description: "), description ? description : "Not found.");
-    console.log("------------");
-    console.log(chalk.italic.green("Summary: "), summary ? summary : "Not found.");
+    let res;
+    try {
+        res = await wiki.summary(search);
+        if (options.debug == "2") console.log(res);
+    } catch (error) {
+        console.error(error);
+    }
+
+
+    const type = res.type;
+    const title = res.title;
+    const summary = res.extract;
+    if (options.copy) {
+        clipboard.writeSync(summary);
+    }
+
+    if (!title || title == "Not found.") {
+        console.log("No results found.");
+    } else if (title != "Not found." && options.browser) {
+        await open(res.content_urls.desktop.page);
+    } else {
+        const description = res.description;
+        console.log(chalk.italic.green("Title: "), title);
+        console.log("------------");
+        console.log(chalk.italic.green("Description: "), description ? description : "Not found.");
+        console.log("------------");
+        console.log(chalk.italic.green("Summary: "), summary ? summary : "Not found.");
+        if (type == "disambiguation") console.log(chalk.italic.yellow(`\nNot what you looked for? Open it in the browser by using:\nOpen it in the browser by using: wikisearch -s ${search} -b`));
+    }
 }
